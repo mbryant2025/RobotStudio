36391f0928569bddb91762ffc6a45cda0f52c7cc
Michael Bryant <mcb115@duke.edu>
2024-03-27 21:05:19 -0400

    email script

---
+++ email_ip.py
@@ -0,0 +1,27 @@
+import smtplib, ssl
+import socket
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    r = s.getsockname()[0]
+    s.close()
+    return r
+
+
+port = 465
+smtp_server = 'smtp.gmail.com'
+sender_email = 'mbryant2025@gmail.com'
+receiver_email = 'mbryant2025@gmail.com'
+password = ''
+message = f'''\
+Subject: Robot IP Update
+
+{get_local_ip()}'''
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
